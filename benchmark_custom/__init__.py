58e614e769289aae4b96585f9f60bc9168131d5d
keim24 <mkeisuke0124@gmail.com>
2021-02-18 12:34:52 +0900

    Delete __init__.py

--- benchmark_custom/__init__.py
+++
@@ -1,6 +0,0 @@
-from .coil_agent import CoILAgent
-# from drive.suites.test_t2_suite import TestT2
-
-from .gaze_agent import GazeAgent
-from .abn_agent import ABNAgent
-from .gl_agent import GLAgent
