5561bd1e61e2135c574c0748b3fb8afa0b3b3a90
keim24 <mkeisuke0124@gmail.com>
2021-02-19 14:12:43 +0900

    Delete GazePrediction directory

--- GazePrediction/__init__ (1).py
+++
@@ -1,4 +0,0 @@
-
-from .conditional_conv_model import CoConvModel
-from .conv_model_cmd import ConvModelCMD
-from .conv_model_cmd_v2 import ConvModelCMD_v2
--- GazePrediction/dataloader/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from .dataloader import CarlaGazeVideoDataset
-from .dataloader_v1 import CarlaGazeDataset
-
